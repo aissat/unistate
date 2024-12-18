9f2826468a4ea5356780c0e14b312fe2b6ddd070
aissat <mr.aissat@gmail.com>
2024-12-18 22:57:17 +0100

    Add unistate_provider library and export relevant provider files

---
+++ unistate_provider/lib/unistate_provider.dart
@@ -0,0 +1,5 @@
+library unistate_provider;
+
+export 'src/provider/unistate.dart';
+export 'src/provider/multi_unistate.dart' hide UniStateInheritedNotifier;
+export 'src/provider/unistate_context.dart';
