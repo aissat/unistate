806dd31cd5e64e349697ff50de673bb63c759aba
aissat <mr.aissat@gmail.com>
2024-12-20 17:06:33 +0100

    Refactor library declaration in `unistate_provider.dart` for improved clarity and organization

--- unistate_provider/lib/unistate_provider.dart
+++ unistate_provider/lib/unistate_provider.dart
@@ -1,5 +1,5 @@
-library unistate_provider;
+library;
 
-export 'src/provider/unistate.dart';
 export 'src/provider/multi_unistate.dart' hide UniStateInheritedNotifier;
+export 'src/provider/unistate.dart';
 export 'src/provider/unistate_context.dart';
