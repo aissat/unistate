8085c0e63214851283a313625b59207918e81103
aissat <mr.aissat@gmail.com>
2024-12-21 02:19:18 +0100

    Remove unused import in `UnistateNotifierMixin` and ensure proper export formatting in `unistate_adapter.dart`

--- unistate_adapter/lib/src/mixin/unistate_notifier_mixin.dart
+++ unistate_adapter/lib/src/mixin/unistate_notifier_mixin.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/foundation.dart';
-// import 'auto_dispose_mixin.dart';
 
 /// Mixin for ChangeNotifier based classes
 mixin UnistateNotifierMixin<T> on ChangeNotifier implements ValueListenable<T> {
