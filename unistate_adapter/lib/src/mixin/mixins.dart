ca4dfb7a351392f3679ad9a6a893f0d2ed07873e
aissat <mr.aissat@gmail.com>
2024-12-22 23:54:43 +0100

    Refactor exports in `mixins.dart` to include `UnistateGetxMixin` and remove duplicates

--- unistate_adapter/lib/src/mixin/mixins.dart
+++ unistate_adapter/lib/src/mixin/mixins.dart
@@ -1,4 +1,5 @@
+export 'unistate_bloc_mixin.dart';
+export 'unistate_cubit_mixin.dart';
+export 'unistate_getx_mixin.dart';
 export 'unistate_listenable_mixin.dart';
 export 'unistate_notifier_mixin.dart';
-export 'unistate_bloc_mixin.dart';
-export 'unistate_cubit_mixin.dart'; 
