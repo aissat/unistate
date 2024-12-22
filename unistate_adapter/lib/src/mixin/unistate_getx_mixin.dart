f6ac0620901e6366bcdb2c80ca764826b77dfcba
aissat <mr.aissat@gmail.com>
2024-12-22 23:49:17 +0100

    Add `UnistateGetxMixin` for `ValueListenable` integration with GetX

---
+++ unistate_adapter/lib/src/mixin/unistate_getx_mixin.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/foundation.dart';
+import 'package:get/get.dart';
+
+/// Mixin for GetX based classes that implements ValueListenable
+mixin UnistateGetxMixin<T extends Object?> on GetxController
+    implements ValueListenable<T> {
+  late final Rx<T> _rx;
+  final List<VoidCallback> _listeners = [];
+  Worker? _worker;
+
+  @override
+  T get value => _rx.value;
+
+  set value(T newState) {
+    _rx.value = newState;
+    // print('emitted: $newState');
+  }
+
+  @override
+  VoidCallback addListener(VoidCallback listener) {
+    // print('addListener');
+    _listeners.add(listener);
+    // print('listeners: ${_listeners.length}');
+
+    // Create worker if it doesn't exist
+    _worker ??= ever(_rx, (_) {
+      // Create a copy of the listeners list to avoid concurrent modification
+      for (var listener in List.of(_listeners)) {
+        listener();
+      }
+    });
+
+    // Return a dispose function
+    return () {
+      removeListener(listener);
+    };
+  }
+
+  void initState(T initialState) {
+    // print('initState');
+    _rx = initialState.obs;
+  }
+
+  @override
+  void onClose() {
+    _worker?.dispose();
+    _worker = null;
+    _listeners.clear();
+    super.onClose();
+  }
+
+  @override
+  void removeListener(VoidCallback listener) {
+    _listeners.remove(listener);
+    if (_listeners.isEmpty) {
+      _worker?.dispose();
+      _worker = null;
+    }
+  }
+}
